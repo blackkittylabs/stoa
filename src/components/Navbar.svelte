138810fcf55bd92a06def6af1e044d234808b58b
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2025-05-05 17:08:17 -0400

    Add main app layout

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+import { page } from "$app/state";
+import { Sparkles, Telescope, Settings } from "@lucide/svelte";
+</script>
+
+<nav>
+  <div class="flex h-5 items-center justify-between text-sm font-semibold">
+    <a
+      class="p-4 ps-0"
+      href="/create"
+      aria-current={page.url.pathname === "/create"}
+    >
+      <Sparkles strokeWidth={page.url.pathname === "/create" ? 2.25 : 2} />
+    </a>
+    <a
+      class="p-4"
+      href="/explore"
+      aria-current={page.url.pathname === "/explore"}
+    >
+      <Telescope strokeWidth={page.url.pathname === "/explore" ? 2.25 : 2} />
+    </a>
+    <a
+      class="p-4 pe-0"
+      href="/settings"
+      aria-current={page.url.pathname === "/settings"}
+    >
+      <Settings strokeWidth={page.url.pathname === "/settings" ? 2.25 : 2} />
+    </a>
+  </div>
+</nav>
+
+<style>
+  a[aria-current="true"] {
+    color: hsl(var(--primary));
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,28 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+import '../app.css';
+import Navbar from '../components/Navbar.svelte';
+const { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex flex-col h-screen">
+  <main class="flex-1 overflow-y-auto">
+    <div class="container mx-auto pt-8">
+      {@render children()}
+      <div class="h-32 md:h-10"></div>
+    </div>
+  </main>
+  <nav class="sticky bottom-0 left-0 right-0 border-t">
+    <div class="container mx-auto py-4">
+      <Navbar />
+    </div>
+  </nav>
+</div>
+
+<style>
+  :global(html, body) {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+</style>
