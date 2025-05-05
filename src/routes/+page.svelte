221e91af86a54de736d7732b6cd00a4f19b29111
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2025-05-05 16:18:15 -0400

    Add Farcaster mini app sdk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+import { sdk } from "@farcaster/frame-sdk";
+import { onMount } from "svelte";
+
+onMount(async () => {
+    await sdk.actions.ready();
+});
+</script>
+
+<h1>Welcome to Stoa</h1>
