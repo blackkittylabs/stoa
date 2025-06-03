a203aa45f9b78224abc321b2a0f8aa5f4da54c5e
dionysuz <13951458+dionysuzx@users.noreply.github.com>
2025-06-03 19:29:04 -0400

    chore: migrate to tailwindcss v4

--- src/lib/components/ui/input-otp/input-otp.svelte
+++ src/lib/components/ui/input-otp/input-otp.svelte
@@ -15,7 +15,7 @@
 	bind:value
 	data-slot="input-otp"
 	class={cn(
-		"has-disabled:opacity-50 flex items-center gap-2 [&_input]:disabled:cursor-not-allowed",
+		"has-disabled:opacity-50 flex items-center gap-2 disabled:[&_input]:cursor-not-allowed",
 		className
 	)}
 	{...restProps}
--- src/lib/components/ui/slider/slider.svelte
+++ src/lib/components/ui/slider/slider.svelte
@@ -21,7 +21,7 @@ get along, so we shut typescript up by casting `value` to `never`.
 	data-slot="slider"
 	{orientation}
 	class={cn(
-		"relative flex w-full touch-none select-none items-center data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col data-[disabled]:opacity-50",
+		"relative flex w-full touch-none select-none items-center data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col data-disabled:opacity-50",
 		className
 	)}
 	{...restProps}
@@ -45,7 +45,7 @@ get along, so we shut typescript up by casting `value` to `never`.
 			<SliderPrimitive.Thumb
 				data-slot="slider-thumb"
 				index={thumb}
-				class="border-primary bg-background ring-ring/50 focus-visible:outline-hidden block size-4 shrink-0 rounded-full border shadow-sm transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 disabled:pointer-events-none disabled:opacity-50"
+				class="border-primary bg-background ring-ring/50 focus-visible:outline-hidden block size-4 shrink-0 rounded-full border shadow-xs transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 disabled:pointer-events-none disabled:opacity-50"
 			/>
 		{/each}
 	{/snippet}
